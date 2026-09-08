dc2377bfc30ba8b54e30acfbd4f35177174298e5
ozperorian <themitsuhashikid@gmail.com>
2026-09-08 15:35:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'kyou-gakkou-v2';
+const CACHE = 'kyou-gakkou-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './sw.js', './cover.png', './story.txt'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('./')));
+  } else {
+    event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  }
+});
